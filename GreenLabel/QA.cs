18f204d3f00453c6f0dc48e839f74f2780a207a7
rveloz9192 <51169144+rveloz9192@users.noreply.github.com>
2019-06-26 21:08:04 -0600

    6.26.19 @ 2107 | Added Printer Selection

--- GreenLabel/QA.cs
+++ GreenLabel/QA.cs
@@ -20,6 +20,8 @@ namespace GreenLabel
         string rcvrNum;
         string rev;
         SqlConnection con = new SqlConnection(sCon);
+        bool printerSelected = false;
+        String printerName;
         String Username { get; }
         //ILabel lbl = DYMO.Label.Framework.Label.Open("C:\\Users\\rveloz\\source\\repos\\roger-maddocks-bi\\GreenLabel\\Resources\\Lbl.label");
         private static readonly string sCon = "Data Source=bldrsyte8db01;Initial Catalog=EM_App;Persist Security Info=True;User ID=Travelmfg;Password=Tr@vel@mfg";
@@ -35,42 +37,49 @@ namespace GreenLabel
 
         private void TxtReceiverNumber_TextChanged(object sender, EventArgs e)
         {
-            rcvrNum = txtReceiverNumber.Text;
-            SqlCommand cmd = new SqlCommand(@"SELECT rcvr_num, item, ref_num, ref_line
-                                            FROM [EM_App].[dbo].[RS_QCRcvr]
-                                            Where rcvr_num = @RCVRNUM", con);
-            cmd.Parameters.AddWithValue("@RCVRNUM", rcvrNum);
-            con.Open();
-            SqlDataAdapter da = new SqlDataAdapter(cmd);
-            DataTable dt = new DataTable();
-
-            da.Fill(dt);
-
-            if (dt.Rows.Count > 0)
+            if (txtReceiverNumber.Text.All(char.IsDigit))
             {
-                string PN = dt.Rows[0][1].ToString();
-                string PO = dt.Rows[0][2].ToString();
-                string POline = dt.Rows[0][3].ToString();
+                rcvrNum = txtReceiverNumber.Text;
+                SqlCommand cmd = new SqlCommand(@"SELECT rcvr_num, item, ref_num, ref_line
+                                                    FROM [EM_App].[dbo].[RS_QCRcvr]
+                                                    Where rcvr_num = @RCVRNUM", con);
+                cmd.Parameters.AddWithValue("@RCVRNUM", rcvrNum);
+                con.Open();
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
 
-                //Command to retrieve revision
-                SqlCommand cmd0 = new SqlCommand(@"SELECT TOP 1 revision
-                                                        FROM [EM_App].[dbo].[poitem_all]
-                                                        where po_num = @PO and item = @PN and po_line = @POline", con);
+                da.Fill(dt);
 
-                cmd0.Parameters.AddWithValue("@PO", PO);
-                cmd0.Parameters.AddWithValue("@PN", PN);
-                cmd0.Parameters.AddWithValue("@POline", POline);
+                if (dt.Rows.Count > 0)
+                {
+                    string PN = dt.Rows[0][1].ToString();
+                    string PO = dt.Rows[0][2].ToString();
+                    string POline = dt.Rows[0][3].ToString();
 
-                SqlDataAdapter da0 = new SqlDataAdapter(cmd0);
-                DataTable dt0 = new DataTable();
+                    //Command to retrieve revision
+                    SqlCommand cmd0 = new SqlCommand(@"SELECT TOP 1 revision
+                                                                    FROM [EM_App].[dbo].[poitem_all]
+                                                                    where po_num = @PO and item = @PN and po_line = @POline", con);
 
-                da0.Fill(dt0);
+                    cmd0.Parameters.AddWithValue("@PO", PO);
+                    cmd0.Parameters.AddWithValue("@PN", PN);
+                    cmd0.Parameters.AddWithValue("@POline", POline);
 
-                rev = dt0.Rows[0][0].ToString();
-                txtRevision.Text = rev;
+                    SqlDataAdapter da0 = new SqlDataAdapter(cmd0);
+                    DataTable dt0 = new DataTable();
+
+                    da0.Fill(dt0);
+
+                    rev = dt0.Rows[0][0].ToString();
+                    txtRevision.Text = rev;
+                }
+
+                con.Close();
             }
-
-            con.Close();
+            else
+            {
+                txtReceiverNumber.Clear();
+            }
         }
 
 
@@ -84,19 +93,19 @@ namespace GreenLabel
             rcvrNum = txtReceiverNumber.Text;
             string numCopies = numberCopies.Value.ToString();
             rev = txtRevision.Text;
-            
 
-            
+
+
 
 
             //Connect to syteline database and command to retrieve Part Number and PO#
-            
+
             SqlCommand cmd1 = new SqlCommand(@"SELECT rcvr_num, item, ref_num, ref_line
                                             FROM [EM_App].[dbo].[RS_QCRcvr]
                                             Where rcvr_num = @RCVRNUM", con);
             cmd1.Parameters.AddWithValue("@RCVRNUM", rcvrNum);
 
-            
+
 
 
             con.Open();
@@ -107,51 +116,57 @@ namespace GreenLabel
 
             // Check to see if receiver number returned information
             if (dt1.Rows.Count > 0)
+            {
+                string PN = dt1.Rows[0][1].ToString();
+                string PO = dt1.Rows[0][2].ToString();
+                string POline = dt1.Rows[0][3].ToString();
+
+
+
+
+                //--------------------------Logic for printing from a Zebra------------------------
+
+                //string s = $"^XA^PW700\n^FO250,50^ADN,30,20^FDBI ACCEPTED^FS\n^FO0,150^ADN,90,40^FDPN:{PN}^FS\n^FO0,250^ADN,50,30^FDPO#:{PO}^FS\n^FO0,310^ADN,90,30^FDDate:{time}^FS\n^FO0,410^ADN,50,30^FDINSP:{Username.Substring(0,2)}^FS\n^FO0,470^ADN,90,30^FDREV:{rev}^FS\n^XZ";
+
+                string s =  $"^XA^PW609^LL406\n" +
+                            $"^FO205,20^CF0,40,40^FDBI ACCEPTED^FS\n" +
+                            $"^FO10,100^CF0,60,40^FDPN:^FS\n" +
+                            $"^FO80,60^CF0,120,60^FD{PN}^FS\n" +
+                            $"^FO10,160^CF0,30,25^FDPO#:^FS\n" +
+                            $"^FO10,220^FDREV:^FS\n" +
+                            $"^FO10,280^FDDATE:^FS\n" +
+                            $"^FO10,340^FDINSP:^FS\n" +
+                            $"^FO80,340^FD{Username.Substring(0, 2).ToUpper()}^FS\n" +
+                            $"^FO80,200^CF0,60,60^FD{rev.ToUpper()}^FS\n" +
+                            $"^FO80,270^FD{DateTime.Today.ToString("MM/dd/yyyy")}^FS\n" +
+                            $"^FO80,160^CF0,30,25^FD{PO}^FS\n" +
+                            $"^PQ{numCopies}\n"+
+                            $"^XZ";
+
+
+
+                if (printerSelected == false)
                 {
-                    string PN = dt1.Rows[0][1].ToString();
-                    string PO = dt1.Rows[0][2].ToString();
-                    string POline = dt1.Rows[0][3].ToString();
-
-                    
-
-
-                    //--------------------------Logic for printing from a Zebra------------------------
-
-                    //string s = $"^XA^PW700\n^FO250,50^ADN,30,20^FDBI ACCEPTED^FS\n^FO0,150^ADN,90,40^FDPN:{PN}^FS\n^FO0,250^ADN,50,30^FDPO#:{PO}^FS\n^FO0,310^ADN,90,30^FDDate:{time}^FS\n^FO0,410^ADN,50,30^FDINSP:{Username.Substring(0,2)}^FS\n^FO0,470^ADN,90,30^FDREV:{rev}^FS\n^XZ";
-
-                    string s =  $"^XA^PW609^LL406\n" +
-                                $"^FO205,20^CF0,40,40^FDBI ACCEPTED^FS\n" +
-                                $"^FO10,100^CF0,60,40^FDPN:^FS\n" +
-                                $"^FO80,60^CF0,120,60^FD{PN}^FS\n" +
-                                $"^FO10,160^CF0,30,25^FDPO#:^FS\n" +
-                                $"^FO10,220^FDREV:^FS\n" +
-                                $"^FO10,280^FDDATE:^FS\n" +
-                                $"^FO10,340^FDINSP:^FS\n" +
-                                $"^FO80,340^FD{Username.Substring(0, 2).ToUpper()}^FS\n" +
-                                $"^FO80,200^CF0,60,60^FD{rev}^FS\n" +
-                                $"^FO80,270^FD{DateTime.Today.ToString("MM/dd/yyyy")}^FS\n" +
-                                $"^FO80,160^CF0,30,25^FD{PO}^FS\n" +
-                                $"^PQ{numCopies}\n"+
-                                $"^XZ";
-
-
                     PrintDialog pd = new PrintDialog();
                     pd.PrinterSettings = new PrinterSettings();
                     if (DialogResult.OK == pd.ShowDialog(this))
                     {
-                        RawPrinterHelper.SendStringToPrinter(pd.PrinterSettings.PrinterName, s);
+                        printerName = pd.PrinterSettings.PrinterName;
+                        printerSelected = true;
                     }
+                }
 
-                    //------------------Logic for printing from a DYNO-------------
-                    //lbl.SetObjectText("lblFirstName", PN);
-                    //lbl.SetObjectText("lblLastName", PO);
-                    //lbl.Print("DYMO LabelWriter 450 Turbo");
-                }
+                if (printerSelected == true) { RawPrinterHelper.SendStringToPrinter(printerName, s); }
+                //------------------Logic for printing from a DYNO-------------
+                //lbl.SetObjectText("lblFirstName", PN);
+                //lbl.SetObjectText("lblLastName", PO);
+                //lbl.Print("DYMO LabelWriter 450 Turbo");
+            }
             else//if receiver number didnt return results
-                {
-                    Console.WriteLine($"Receiver Number {rcvrNum} not found");
-                    MessageBox.Show("Receiver Number " + rcvrNum + " was not found", "ERROR");
-                }
+            {
+                Console.WriteLine($"Receiver Number {rcvrNum} not found");
+                MessageBox.Show("Receiver Number " + rcvrNum + " was not found", "ERROR");
+            }
 
 
             con.Close();
